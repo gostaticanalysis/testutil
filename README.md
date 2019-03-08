04cf0f1ffa4920522a57517914e0622959b12c32
Takuya Ueda <uedatakuya@gmail.com>
2019-03-09 01:21:44 +0900

    Add filter

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # testutil
-Library: Utilities for test of static analysis tools
+[![godoc.org][godoc-badge]][godoc]
+
+Utilities for test of static analysis tools.
+
+<!-- links -->
+[godoc]: https://godoc.org/github.com/gostaticanalysis/testutil
+[godoc-badge]: https://img.shields.io/badge/godoc-reference-4F73B3.svg?style=flat-square&label=%20godoc.org
+
---
+++ filter.go
@@ -0,0 +1,22 @@
+package testutil
+
+import "golang.org/x/tools/go/analysis/analysistest"
+
+// ErrorfFunc implements analysistest.Testing.
+type ErrorfFunc func(format string, args ...interface{})
+
+var _ analysistest.Testing = ErrorfFunc(nil)
+
+// Errorf implements analysistest.Testing.
+func (f ErrorfFunc) Errorf(format string, args ...interface{}) {
+	f(format, args...)
+}
+
+// WithFilter calls t.Errorf when the filter returns true.
+func WithFilter(t analysistest.Testing, filter func(format string, args ...interface{}) bool) analysistest.Testing {
+	return ErrorfFunc(func(format string, args ...interface{}) {
+		if filter(format, args...) {
+			t.Errorf(format, args...)
+		}
+	})
+}
